75f3a530343a52efdeb1806c9d6764d3b5c44af4
Abhilash Kumar Jha <152012438+JHA123ABHIlash@users.noreply.github.com>
2026-07-11 09:49:09 +0530

    Time: 0 ms (100%), Space: 54.8 MB (8.89%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let left=0;
+    let right=nums.length-1;
+    let ans=nums[0];
+
+    while(left<=right){
+        let mid=Math.floor(left+(right-left)/2);
+
+        if(nums[left]<=nums[mid]){
+            ans=Math.min(ans,nums[left]);
+            left=mid+1; 
+        }else{
+            ans=Math.min(ans,nums[mid]);
+            right=mid-1;
+        }
+    }
+    return ans;
+};
