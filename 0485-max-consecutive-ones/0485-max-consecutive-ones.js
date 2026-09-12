88c04716d5722272388e914891ed3ddc8d8fa440
Abhilash Kumar Jha <152012438+JHA123ABHIlash@users.noreply.github.com>
2026-09-12 10:51:59 +0530

    Time: 2 ms (66.82%), Space: 59.1 MB (62.28%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.js
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -6,12 +6,11 @@ var findMaxConsecutiveOnes = function(nums) {
     let cnt=0;
     let cnt1=0;
 
-    for(let i=0; i<nums.length; i++){
+    for(let i=0;i<nums.length;i++){
         if(nums[i]==1){
             cnt++;
-            cnt1=Math.max(cnt1,cnt);
+            cnt1=Math.max(cnt,cnt1);
         }else{
-            
             cnt=0;
         }
     }
