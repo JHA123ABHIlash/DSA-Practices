27617e8f6382e6c8de18d30a21f7ecc60bee3693
Abhilash Kumar Jha <152012438+JHA123ABHIlash@users.noreply.github.com>
2026-09-22 21:14:49 +0530

    Time: 0 ms (100%), Space: 53.6 MB (91.32%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    let running =[];
+    let sum=0;
+
+    for(let i=0;i<nums.length;i++){
+        sum+=nums[i];
+
+        running[i]=sum;
+    }
+    return running;
+};
